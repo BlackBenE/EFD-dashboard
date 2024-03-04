8d5ed9333ec3c21481000bd7f66abe22ce510ae7
BlackBenE <120026521+BlackBenE@users.noreply.github.com>
2024-03-04 02:56:09 +0100

    update crud round

---
+++ EFD-dashboard/TourDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TourDetailsViewController.swift
+//  EFD-dashboard
+//
+//  Created by Elie Bengou on 04/03/2024.
+//
+
+import UIKit
+
+class TourDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
